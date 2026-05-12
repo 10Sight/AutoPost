36bb4e19d05632829b74aac358140de7eea0ff86
Jatin Kumar Nagar <jatinnagar563@gmail.com>
2026-05-12 18:41:22 +0530

    Security: Broaden CSP to support all secure media and socket connections

--- server/src/app.js
+++ server/src/app.js
@@ -72,8 +72,8 @@ app.use(
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
                 "script-src": ["'self'", "'unsafe-inline'", "https://*.razorpay.com", "https://checkout.razorpay.com", "https://accounts.google.com"],
                 "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
-                "img-src": ["'self'", "data:", "https://res.cloudinary.com", "https://*.googleusercontent.com", "https://*.fbcdn.net", "https://*.licdn.com", "https://*.linkedin.com", "https://*.twimg.com"],
-                "connect-src": ["'self'", "https://api.cloudinary.com", "https://*.razorpay.com", "https://luna.razorpay.com", "https://lumberjack.razorpay.com"],
+                "img-src": ["'self'", "data:", "blob:", "https:"],
+                "connect-src": ["'self'", "blob:", "https:", "wss:", "https://api.cloudinary.com", "https://*.razorpay.com"],
                 "frame-src": ["'self'", "https://*.razorpay.com", "https://checkout.razorpay.com", "https://accounts.google.com"],
                 "font-src": ["'self'", "https://fonts.gstatic.com"]
             },
